49224f7b634a32c50c7f288727967631a3fe113d
Emanuele Ingrosso <ingro85@gmail.com>
2020-12-24 16:17:34 +0100

    Fix default model's namespace for Laravel 8.x

--- src/Base/Controller.php
+++ src/Base/Controller.php
@@ -107,7 +107,7 @@ class Controller extends BaseController
         $domain = $this->getRootNamespace($ns);
         $name = $this->getRelatedClassName('Controller', '', $ns);
 
-        return $domain . '\\' . $name;
+        return $domain . '\\Model\\' . $name;
     }
 
     /**
